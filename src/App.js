00e85447df969743820099443b88de28e3878437
LeFortgerman <lefortgerman@hotmail.com>
2022-08-24 10:24:15 -0300

    subida trabajo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Bebidas from './componentes/Bebidas.js';
+import NavBar from './componentes/navBar';
+const App = () => {
   return (
-    <div className="App">
-      <header>
-        <nav className='App-header'>
-              <a>Quienes Somos</a>
-            
-              <a>Contactos</a>
-              
-              <a>personal</a>
+    <div className="">
+      <NavBar/>
     
-        </nav>
-       
-      </header>
-      <section className='productos-container' >
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" href="Energizantes.html">Energizantes</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="saludables.html">saludables</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="Diversion.html">Diversion</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="Relajacion.html">Relajacion</a>
-        </li>
-      </ul>
-        </div>
-        
-      </section>
+    
 
+    <Bebidas/>
+    
+  
     </div>
   );
 }
