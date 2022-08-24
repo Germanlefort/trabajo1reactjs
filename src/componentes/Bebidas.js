6db0a53fdad319e57dfc3d8df1c956518f738ea0
LeFortgerman <lefortgerman@hotmail.com>
2022-08-24 09:59:35 -0300

    subida trabajo2

---
+++ src/componentes/Bebidas.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+const Bebidas = () => {
+
+return( 
+    <section className="productos-container">
+        <h3>Productos</h3>
+        <hr/>
+        <ul>
+            <h4> BEBIDAS</h4>
+            <li>Energizantes</li>
+            <li>Relajacion</li>
+            <li>Diversion</li>
+        </ul>
+    </section>
+
+)
+}
+ export default Bebidas
---
+++ src/componentes/navBar.js
@@ -0,0 +1,22 @@
+
+
+
+const Navbar = () => {
+return (
+    <header>
+      <h2 className="titulo-contenido">CAN CAB BEBIDAS</h2>
+
+      <nav className="Nav-Bar">
+      <a href="contacto.html"> Contactos </a>
+        <hr></hr>
+      <a href="quienesosmos.html"> Quienes Somos</a>
+        <hr></hr>
+      <a href="productos.html"> Productos</a>  
+        <hr></hr>
+          </nav>  
+    </header>
+
+)
+
+}
+export default Navbar
